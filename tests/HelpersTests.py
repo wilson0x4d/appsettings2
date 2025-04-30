49667e2e6123ac7f56ee68d102f7f8322871a05f
wilson0x4d <mrshaunwilson@msn.com>
2025-04-30 17:50:37 -0500

    improve(tests): add regression tests for recently fixed defect

---
+++ tests/HelpersTests.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: Copyright (C) Shaun Wilson
+# SPDX-License-Identifier: MIT
+
+import appsettings2
+from punit import *
+
+@fact
+def getConfigurationLoadsVariations() -> None:
+    """verify that documented variations are loaded by default."""
+    config = appsettings2.getConfiguration(baseName='tests/configs/appsettings')
+    assert True == config.default
+    assert True == config.prod
+    assert True == config.staging
+    assert True == config.qa
+    assert True == config.dev
+    assert True == config.local
+
+@fact
+def getConfigurationCanDisableVariations() -> None:
+    """verify that we can disable loading of config variations by parameter."""
+    config = appsettings2.getConfiguration(baseName='tests/configs/appsettings', variations=None)
+    assert True == config.default # NOTE: default/non-varied config is still expected to load
+    assert False == config.get('prod', False)
+    assert False == config.get('staging', False)
+    assert False == config.get('qa', False)
+    assert False == config.get('dev', False)
+    assert False == config.get('local', False)
