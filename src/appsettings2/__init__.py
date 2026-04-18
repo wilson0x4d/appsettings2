8678ae768a641e9ca760ab8b38b2eda2e6f85a63
wilson0x4d <mrshaunwilson@msn.com>
2026-04-18 11:50:07 -0500

    improve: bake commit hash

--- src/appsettings2/__init__.py
+++ src/appsettings2/__init__.py
@@ -1,9 +1,6 @@
 # SPDX-FileCopyrightText: © 2025 Shaun Wilson
 # SPDX-License-Identifier: MIT
 
-__version__ = '0.0.0'
-__commit__ = '0abc123'
-
 from .Configuration import Configuration
 from .ConfigurationBuilder import ConfigurationBuilder
 from .ConfigurationException import ConfigurationException
@@ -12,6 +9,8 @@ from . import helpers
 from . import providers
 
 
+__version__ = '0.0.0'
+__commit__ = '0abc123'
 __all__ = [
     '__version__', '__commit__',
     'Configuration',
